8c3e96a35cc2744c416b392d7aeba11f0ca8a7cc
Taranis Hunter <tvhunter@ucsc.edu>
2019-04-16 00:22:07 -0700

    added jsonify

--- app.py
+++ app.py
@@ -4,16 +4,25 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+## returns either work, or rip
 class key_value(Resource):
 
 	def get(self, key):
 		thisdict = {'test': 'tester'}
 		if key in thisdict:
-			return make_response('work', 200)
+			return make_response(jsonify(doesExist=True, message="Retrieved successfully", value="Data Structures"), 200)
 		else:
-			return make_response('rip',200)
+			return make_response(jsonify(doesExist=False, error="Key does not exist", message="Error in GET"), 404)
+
+	def post(self, key):
+		thisdict = {'test': 'tester'}
+		if key in thisdict:
+			return make_response(jsonify(message="Added successfully",replaced=false), 201)
+		else:
+			return make_response(jsonify(message="Updated successfully",replaced=true), 200)
+
 
 api.add_resource(key_value, '/key-value-store/<key>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8082)
