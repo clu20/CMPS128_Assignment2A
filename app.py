4bb887726d9ecb723849e4e18e9f20c1cabd9aad
Taranis Hunter <tvhunter@ucsc.edu>
2019-04-16 13:00:23 -0700

    tarans branch

--- app.py
+++ app.py
@@ -14,13 +14,25 @@ class key_value(Resource):
 		else:
 			return make_response(jsonify(doesExist=False, error="Key does not exist", message="Error in GET"), 404)
 
-	def post(self, key):
+	def put(self, key):
 		thisdict = {'test': 'tester'}
-		if key in thisdict:
-			return make_response(jsonify(message="Added successfully",replaced=false), 201)
+		if len(key) < 50:
+			message = request.get_json()
+			if key in thisdict: ## edit the message
+				if message.get('value'):## data exists
+					thisdict[key] = message.get('value')
+					return make_response(jsonify(message="Updated successfully",replaced=True), 200)
+				else: ## data nonexistent
+					return make_response(jsonify(error="Value is missing",message="Error in PUT"), 400)
+			else: ##add a new message at key
+				if message.get('value'): ##data exists
+					##change message of 'key'
+					thisdict[key] = message.get('value')
+					return make_response(jsonify(message="Added successfully",replaced=False), 201)
+				else: ##data nonexistent
+					return make_response(jsonify(error="Value is missing",message="Error in PUT"), 400)
 		else:
-			return make_response(jsonify(message="Updated successfully",replaced=true), 200)
-
+			return make_response(jsonify(error="Key is too long", message="Error in PUT"), 400)
 
 api.add_resource(key_value, '/key-value-store/<key>')
 
